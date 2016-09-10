ba6f43dfd2c8975eaf56742024b26c14bf25c375
Ali Dehghani <ali.dehghani.g@gmail.com>
2016-09-11 00:18:49 +0430

    First commit for sockets on heroku
    Signed-off-by: Ali Dehghani <ali.dehghani.g@gmail.com>

---
+++ src/main/java/me/alidg/config/WebSocketConfiguration.java
@@ -0,0 +1,18 @@
+package me.alidg.config;
+
+import me.alidg.sockets.EchoSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry
+                .addHandler(new EchoSocketHandler(), "/echo")
+                .setAllowedOrigins("*");
+    }
+}
---
+++ src/main/java/me/alidg/sockets/EchoSocketHandler.java
@@ -0,0 +1,12 @@
+package me.alidg.sockets;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class EchoSocketHandler extends TextWebSocketHandler {
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        session.sendMessage(message);
+    }
+}
